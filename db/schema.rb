7204ee03008a8618c3e141b4181fae5a65d507db
nahnahAJ <nahnahahjuwah@gmail.com>
2023-07-11 19:32:48 +0000

    Add users as a foreign key to the properties  table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_204528) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_100327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_204528) do
     t.integer "no_of_bedrooms"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_204528) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "properties", "users"
 end
