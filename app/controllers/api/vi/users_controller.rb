9d8c71bd8cdc59af996150bf89cd313451eb98c5
nahnahAJ <nahnahahjuwah@gmail.com>
2023-07-14 15:41:36 +0000

    Edit the user model

--- app/controllers/api/vi/users_controller.rb
+++ app/controllers/api/vi/users_controller.rb
@@ -36,38 +36,10 @@ class Api::Vi::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password_digest)
+    params.require(:user).permit(:name, :email, :password)
   end
 
   def render_not_found_response
     render json: { error: 'User not found' }, status: :not_found
-  end
-
-  def render_unprocessable_entity_response(invalid)
-    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
-  end
-
-  def render_unauthorized_response
-    render json: { error: 'Unauthorized' }, status: :unauthorized
-  end
-
-  def render_no_content_response
-    render json: { error: 'No content' }, status: :no_content
-  end
-
-  def render_created_response
-    render json: { error: 'Created' }, status: :created
-  end
-
-  def render_ok_response
-    render json: { error: 'Ok' }, status: :ok
-  end
-
-  def render_bad_request_response
-    render json: { error: 'Bad request' }, status: :bad_request
-  end
-
-  def render_forbidden_response
-    render json: { error: 'Forbidden' }, status: :forbidden
   end
 end
