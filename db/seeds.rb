229d6ed906f440da361b72cbd8986ca0d00f4c73
nahnahAJ <nahnahahjuwah@gmail.com>
2023-07-14 15:37:01 +0000

    Add dummy data in the seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# _type: "Duplex", image: "https://images.unsplash.com/photo-1611179836928-5b7c0c4b0b0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZHVwbGV4JTIwYmFja2dyb3VuZCUyMHBhcGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80", price: 100000, category: "Rent", no_of_bedrooms: 4, user_id: 1)
-userTwo = User.create(name: "Jane Doe", email: "jdoe@gmail.com", password_digest: "password")
-propertyTwo = Property.create(description: "A beautiful house", location: "Lagos", property_type: "Duplex", image: "https://images.unsplash.com/photo-1611179836928-5b7c0c4b0b0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZHVwbGV4JTIwYmFja2dyb3VuZCUyMHBhcGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80", price: 100000, category: "Rent", no_of_bedrooms: 4, user_id: 2)
+
+user1 = User.create(name: "John Doe", email: "john@example.com", password: "password")
+user2 = User.create(name: "Jane Doe", email: "jack@example.com", password: "password")
+user3 = User.create(name: "Jack Doe", email: "janew@example.com", password: "password")
+user4 = User.create(name: "Jill Doe", email: "joe@example.com", password: "password")
