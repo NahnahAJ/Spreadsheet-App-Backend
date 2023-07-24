bbb8ca4fbb73fcc58428a51dad6a22fee588d5b7
nahnahAJ <nahnahahjuwah@gmail.com>
2023-07-24 15:31:56 +0000

    Modify the users controller and model i created initially

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+  # has_secure_password
   has_many :properties
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :password_digest, presence: true
 end
