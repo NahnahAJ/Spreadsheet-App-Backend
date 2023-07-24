e6271b183fa8a2590d93b9a14961dd994a34f141
nahnahAJ <nahnahahjuwah@gmail.com>
2023-07-24 15:35:15 +0000

    Define all required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
+  get '/current_user', to: 'current_user#index'
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
     namespace :vi do
-      resources :users, only: [:index, :create, :show, :update, :destroy]
+      # resources :users, only: [:index, :create, :show, :update, :destroy]
       resources :properties, only: [:index, :create, :show, :update, :destroy]
     end
   end
